85d45a852e4b6a0ca45a4558588bca0a5af82d09
Alice Cazati <alice.cazati@gmail.com>
2025-08-13 10:22:53 -0300

    selecao'
    selecao

---
+++ selecao.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+
+
+class selecao{
+
+    public static void ordenacaoselecao(int n,int []num){
+        
+        for(int i=0;i<n;i++){
+            int menor=i;
+            for(int j=i+1;j<5;j++){
+                if(num[menor]>num[j]){
+                    menor=j;
+                }
+            }
+            int temp=num[i];
+            num[i]=num[menor];
+            num[menor]=temp;
+        }
+    }
+     public static void main(String[] args) {
+           int n;
+           Scanner sc = new Scanner(System.in);
+           n=sc.nextInt();
+           int [] num= new int[n];
+           for(int i=0;i<n;i++){
+            num[i]=sc.nextInt();
+           }
+
+           ordenacaoselecao(n,num);
+           for (int m : num) {
+            System.out.print(m + " ");
+        }
+    }
+    
+     
+}
